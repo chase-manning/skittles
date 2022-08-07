1b9e118504f837db498b802b1b2b3d7c98fd1694
chase-manning <chase@manning.dev>
2022-08-07 13:58:32 +0100

    ✅ fix ast test

--- __tests__/test_get-ast.ts
+++ __tests__/test_get-ast.ts
@@ -4,7 +4,6 @@ describe("Get AST", () => {
   it("should work for hello world", async () => {
     const response = getAst("./contracts/hello-world.ts");
     expect(response).not.toBeUndefined();
-    console.log(response);
-    expect(response!.end).toBe(138);
+    expect(response!.end).toBe(146);
   });
 });
