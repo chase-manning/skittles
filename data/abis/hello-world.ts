f97830d8c69eb85e317532b1516f1602383876e3
chase-manning <chase@manning.dev>
2022-08-07 21:01:36 +0100

    :sparkles: add hello world abi

---
+++ data/abis/hello-world.ts
@@ -0,0 +1,30 @@
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "addBalance",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "balance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export default abi;
