bebb7156ce8f264edb5d01ac131d6fd5990b7e03
chase-manning <chase@manning.dev>
2022-08-08 23:24:51 +0100

    :art: change get selector to not require params

--- __tests__/test_get-selector.ts
+++ __tests__/test_get-selector.ts
@@ -5,12 +5,12 @@ import getAbi from "../lib/get-abi";
 describe("Get Selector", () => {
   it("should get balance selector for Hello World", async () => {
     const abi = getAbi("./contracts/hello-world.ts");
-    const selector = getSelector(abi, "balance", []);
+    const selector = getSelector(abi, "balance");
     expect(selector).toBe("0xb69ef8a8");
   });
   it("should get addBalance selector for Hello World", async () => {
     const abi = getAbi("./contracts/hello-world.ts");
-    const selector = getSelector(abi, "addBalance", [1]);
+    const selector = getSelector(abi, "addBalance");
     expect(selector).toBe("0xd91921ed");
   });
 });
--- lib/get-selector.ts
+++ lib/get-selector.ts
@@ -1,7 +1,20 @@
 import { utils } from "ethers";
+import { AbiParameter } from "./get-abi";
 
-const getSelector = (abi: any[], func: string, params: any[]) => {
+const exampleValues: Record<string, any> = {
+  uint256: 1,
+  bool: true,
+  string: "hello",
+  address: "0x1234567890123456789012345678901234567890",
+};
+
+const getSelector = (abi: any[], func: string) => {
   const iface = new utils.Interface(abi);
+  const abiFunction = abi.find((f) => f.name === func);
+  if (!abiFunction) throw new Error(`Could not find function ${func}`);
+  const params = abiFunction.inputs.map(
+    (input: AbiParameter) => exampleValues[input.type]
+  );
   const data = iface.encodeFunctionData(func, params);
   return data.substring(0, 10);
 };
