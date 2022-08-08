c17539810d998f8034714c5f328aa5636465fd76
chase-manning <chase@manning.dev>
2022-08-08 20:18:02 +0100

    :rocket: get yul in main script

--- index.ts
+++ index.ts
@@ -1,8 +1,3 @@
-import getAbi from "./lib/get-abi";
-import { utils } from "ethers";
+import getYul from "./lib/get-yul";
 
-const abi = getAbi("./contracts/hello-world.ts");
-
-const iface = new utils.Interface(abi);
-console.log("Balance", iface.encodeFunctionData("balance", []));
-console.log("Add Balance", iface.encodeFunctionData("addBalance", [1]));
+getYul("./contracts/hello-world.ts");
