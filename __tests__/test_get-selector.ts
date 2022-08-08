e7f527d7fec45acb14b29ce3cf62ed59d258e475
chase-manning <chase@manning.dev>
2022-08-08 17:59:33 +0100

    :art: change selector tests to use getabi

--- __tests__/test_get-selector.ts
+++ __tests__/test_get-selector.ts
@@ -1,13 +1,15 @@
 import getSelector from "../lib/get-selector";
 
-import abi from "../data/abis/hello-world";
+import getAbi from "../lib/get-abi";
 
 describe("Get Selector", () => {
   it("should get balance selector for Hello World", async () => {
+    const abi = getAbi("./contracts/hello-world.ts");
     const selector = getSelector(abi, "balance", []);
     expect(selector).toBe("0xb69ef8a8");
   });
   it("should get addBalance selector for Hello World", async () => {
+    const abi = getAbi("./contracts/hello-world.ts");
     const selector = getSelector(abi, "addBalance", [1]);
     expect(selector).toBe("0xd91921ed");
   });
