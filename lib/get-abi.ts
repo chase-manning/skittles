393c44472bfe981c2f92d39c3614c2fdbe8e96b8
chase-manning <chase@manning.dev>
2022-08-08 17:50:38 +0100

    :sparkles: add get abi function to lib

--- lib/get-abi.ts
+++ lib/get-abi.ts
@@ -1,10 +1,124 @@
 import getAst from "./get-ast";
 
 import abi from "../data/abis/hello-world";
+import {
+  forEachChild,
+  isMethodDeclaration,
+  isParameter,
+  isPropertyDeclaration,
+  MethodDeclaration,
+  Node,
+  ParameterDeclaration,
+  PropertyDeclaration,
+  SourceFile,
+  SyntaxKind,
+} from "typescript";
 
-const getAbi = (file: string): any[] => {
-  const ast = getAst(file);
+export interface AbiParameter {
+  name: string;
+  type: string; // TODO Add subtypes
+}
+
+export interface AbiFunction {
+  type: "function" | "constructor" | "receive" | "fallback";
+  name: string;
+  inputs: AbiParameter[];
+  outputs: AbiParameter[];
+  stateMutability: "view" | "payable" | "nonpayable" | "pure";
+}
+
+export type Abi = AbiFunction[];
+
+const getType = (typeNode: Node | undefined): string => {
+  if (!typeNode) return "void";
+  if (typeNode.kind === SyntaxKind.NumberKeyword) {
+    return "uint256";
+  }
+  if (typeNode.kind === SyntaxKind.VoidKeyword) {
+    return "void";
+  }
+  // TODO Add more types
+  throw new Error("Unsupported type");
+};
+
+const getOutputs = (
+  node: MethodDeclaration | PropertyDeclaration
+): AbiParameter[] => {
+  const type = getType(node.type);
+  if (type === "void") return [];
+  // TODO Support multiple outputs
+  return [
+    {
+      name: "",
+      type,
+    },
+  ];
+};
+
+const getInput = (parameter: ParameterDeclaration): AbiParameter => {
+  return {
+    name: (parameter.name as any).escapedText,
+    type: "uint256",
+  };
+};
+
+const getInputs = (node: MethodDeclaration): AbiParameter[] => {
+  const inputs: AbiParameter[] = [];
+  forEachChild(node, (node) => {
+    if (isParameter(node)) {
+      inputs.push(getInput(node));
+    }
+  });
+  return inputs;
+};
+
+const propertyDeclarationToAbi = (node: PropertyDeclaration): AbiFunction => {
+  return {
+    type: "function",
+    name: (node.name as any).escapedText,
+    inputs: [],
+    outputs: getOutputs(node),
+    stateMutability: "view",
+  };
+};
+
+const methodDeclarationToAbi = (node: MethodDeclaration): AbiFunction => {
+  return {
+    type: "function",
+    name: (node.name as any).escapedText,
+    inputs: getInputs(node),
+    outputs: getOutputs(node),
+    stateMutability: "nonpayable", // TODO Work out if it's payable or not
+  };
+};
+
+const processNode = (node: Node): Abi => {
+  if (isPropertyDeclaration(node)) {
+    return [propertyDeclarationToAbi(node)];
+  }
+  if (isMethodDeclaration(node)) {
+    return [methodDeclarationToAbi(node)];
+  }
+  // TODO Add events
+  // TODO Add errors
+  const abi: Abi = [];
+  node.forEachChild((node) => {
+    abi.push(...processNode(node));
+  });
   return abi;
+};
+
+const processAst = (sourceFile: SourceFile) => {
+  const abi: Abi = [];
+  sourceFile.forEachChild((node) => {
+    abi.push(...processNode(node));
+  });
+  return abi;
+};
+
+const getAbi = (file: string): Abi => {
+  const ast = getAst(file);
+  return processAst(ast);
 };
 
 export default getAbi;
