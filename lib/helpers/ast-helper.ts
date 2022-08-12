ea9623a349bea48048f9ca05346094a4c8a7aaf5
chase-manning <chase@manning.dev>
2022-08-12 01:03:30 +0100

    :coffin: remove unused functions

--- lib/helpers/ast-helper.ts
+++ lib/helpers/ast-helper.ts
@@ -42,29 +42,8 @@ export const getNodeReturnType = (node: Node): string => {
   throw new Error("Unsupported type");
 };
 
-export const getNodeOutputs = (node: Node): string[] => {
-  const type = getNodeReturnType(node);
-  if (type === "void") return [];
-  // TODO Support multiple outputs
-  return [type];
-};
-
-export const getNodeInputs = (node: Node): AbiParameter[] => {
-  const inputs: AbiParameter[] = [];
-  forEachChild(node, (node) => {
-    if (isParameter(node)) {
-      inputs.push({ name: getNodeName(node), type: "uint256" }); // TODO Get the type for real
-    }
-  });
-  return inputs;
-};
-
 export const isPlusEquals = (expression: BinaryExpression): boolean => {
   return expression.operatorToken.kind === SyntaxKind.PlusEqualsToken;
-};
-
-export const isAsteriskToken = (expression: BinaryExpression): boolean => {
-  return expression.operatorToken.kind === SyntaxKind.AsteriskToken;
 };
 
 export const isEquals = (expression: BinaryExpression): boolean => {
