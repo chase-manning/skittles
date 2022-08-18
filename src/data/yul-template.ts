6686b73120fe35306e836f6bae50ad0a2782f807
chase-manning <chase@manning.dev>
2022-08-18 12:45:54 +0100

    :art: minor code formatting

--- src/data/yul-template.ts
+++ src/data/yul-template.ts
@@ -22,6 +22,7 @@ const yulTemplate: string[] = [
   `        datacopy(0, dataoffset("runtime"), datasize("runtime"))`,
   `        return(0, datasize("runtime"))`,
   `    }`,
+  ``,
   `    object "runtime" {`,
   `        code {`,
   `            switch selector()`,
