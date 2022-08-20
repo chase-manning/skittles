7a00abc9636ce005a7f296932e29b1b3eaa85287
chase-manning <chase@manning.dev>
2022-08-20 13:02:10 +0100

    :rocket: add all static variables and classes

--- src/types/core-types.ts
+++ src/types/core-types.ts
@@ -1,1 +1,66 @@
 export type Address = string;
+
+export const self: Address = "0x00000000006c3852cbEf3e08E8dF289169EdE581";
+
+export class block {
+  // Current block miner’s address
+  static get coinbase(): Address {
+    return "0x0000000000000000000000000000000000000000";
+  }
+
+  // Current block difficulty
+  static get difficulty(): number {
+    return 0;
+  }
+
+  // Current block number
+  static get block(): number {
+    return 0;
+  }
+
+  // Equivalent to blockhash(block.number - 1)
+  static get prevhash(): number {
+    return 0;
+  }
+
+  // Current block epoch timestamp
+  static get timestamp(): number {
+    return 0;
+  }
+}
+
+export class chain {
+  // Chain ID
+  static get id(): number {
+    return 0;
+  }
+}
+
+export class msg {
+  // Message data
+  static get data(): string {
+    return "";
+  }
+
+  // Sender of the message (current call)
+  static get sender(): Address {
+    return "0x0000000000000000000000000000000000000000";
+  }
+
+  // Number of wei sent with the message
+  static get value(): number {
+    return 0;
+  }
+}
+
+export class tx {
+  // Gas price of current transaction in wei
+  static get gasprice(): number {
+    return 0;
+  }
+
+  // Sender of the transaction (full call chain)
+  static get origin(): Address {
+    return "0x0000000000000000000000000000000000000000";
+  }
+}
