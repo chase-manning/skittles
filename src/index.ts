66a7a37139e5ecf7f4d9654cabfd9e3d6a268ed6
chase-manning <chase@manning.dev>
2022-08-22 22:50:10 +0100

    :art: move skittles compile into index

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@
 // import figlet from "figlet";
 // import path from "path";
 import { program } from "commander";
-import { skittlesCompile } from "./skittles";
+import getAbi from "./abi/get-abi";
+import getBytecode from "./bytecode/get-bytecode";
+import { getAllContractFiles, writeFile } from "./helpers/file-helper";
+import getSkittlesClass from "./skittles-class/get-skittles-class";
+import getYul from "./yul/get-yul";
 
 // clear();
 // console.log(
@@ -32,3 +36,18 @@ program
   });
 
 program.parse();
+
+const skittlesCompile = async () => {
+  const files = getAllContractFiles();
+  const promises = files.map(async (file) => {
+    const skittlesClass = getSkittlesClass(file);
+    const abi = getAbi(skittlesClass);
+    const { name } = skittlesClass;
+    writeFile("abi", name, JSON.stringify(abi, null, 2));
+    const yul = getYul(skittlesClass, abi);
+    writeFile("yul", name, yul);
+    const bytecode = getBytecode(name, yul);
+    writeFile("bytecode", name, bytecode);
+  });
+  await Promise.all(promises);
+};
