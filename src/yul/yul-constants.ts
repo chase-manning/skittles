e29191c21a80d6990906ce075c5b1479f8ac939a
chase-manning <chase@manning.dev>
2022-08-22 23:01:14 +0100

    :art: move yul constants into separate file

---
+++ src/yul/yul-constants.ts
@@ -0,0 +1,33 @@
+export const decoderFunctions: Record<string, string> = {
+  address: "decodeAsAddress",
+  uint256: "decodeAsUint",
+};
+
+export const returnFunctions: Record<string, string> = {
+  uint256: "return256",
+  bool: "returnBoolean",
+  address: "return256",
+  string: "returnString",
+};
+
+export const evmDialects: Record<string, Record<string, string>> = {
+  block: {
+    coinbase: "coinbase()",
+    difficulty: "difficulty()",
+    block: "number()",
+    prevhash: "",
+    timestamp: "timestamp()",
+  },
+  chain: {
+    id: "chainid()",
+  },
+  msg: {
+    data: "",
+    sender: "caller()",
+    value: "callvalue()",
+  },
+  tx: {
+    gasPrice: "gasprice()",
+    origin: "origin()",
+  },
+};
