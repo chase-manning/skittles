7852b4de01230c031a395a713c6191390a0a4075
chase-manning <chase@manning.dev>
2022-08-22 22:38:37 +0100

    :sparkles: add get skittles statements

---
+++ src/skittles-class/get-skittles-statements.ts
@@ -0,0 +1,225 @@
+import {
+  isBinaryExpression,
+  isBlock,
+  isCallExpression,
+  isElementAccessExpression,
+  isExpressionStatement,
+  isIfStatement,
+  isNewExpression,
+  isPropertyAccessExpression,
+  isReturnStatement,
+  isThrowStatement,
+  Node,
+  Statement,
+} from "typescript";
+import {
+  getNodeName,
+  isEquals,
+  isFalseKeyword,
+  isMinusEquals,
+  isPlusEquals,
+  isTrueKeyword,
+} from "../helpers/ast-helper";
+import {
+  SkittlesExpression,
+  SkittlesExpressionType,
+  SkittlesOperator,
+  SkittlesStatement,
+  SkittlesStatementType,
+} from "../types/skittles-class";
+import getSkittlesExpression from "./get-skittles-expression";
+
+const getSkittlesStatements = (
+  block: Statement | undefined,
+  returnType: string
+): SkittlesStatement[] => {
+  if (!block) return [];
+  if (isBlock(block)) {
+    const { statements } = block;
+    return statements.map((statement: Node) =>
+      getSkittlesStatement(statement, returnType)
+    );
+  }
+  return [getSkittlesStatement(block, returnType)];
+};
+
+const getSkittlesStatement = (
+  node: Node,
+  returnType: string
+): SkittlesStatement => {
+  if (isExpressionStatement(node)) {
+    const { expression } = node;
+    if (isBinaryExpression(expression)) {
+      if (isPropertyAccessExpression(expression.left)) {
+        if (isEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.StorageUpdate,
+            variable: getNodeName(expression.left),
+            value: getSkittlesExpression(expression.right),
+          };
+        }
+        if (isPlusEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.StorageUpdate,
+            variable: getNodeName(expression.left),
+            value: {
+              expressionType: SkittlesExpressionType.Binary,
+              operator: SkittlesOperator.Plus,
+              left: getSkittlesExpression(expression.left),
+              right: getSkittlesExpression(expression.right),
+            },
+          };
+        }
+        if (isMinusEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.StorageUpdate,
+            variable: getNodeName(expression.left),
+            value: {
+              expressionType: SkittlesExpressionType.Binary,
+              operator: SkittlesOperator.Minus,
+              left: getSkittlesExpression(expression.left),
+              right: getSkittlesExpression(expression.right),
+            },
+          };
+        }
+        throw new Error(
+          `Unknown binary expression: ${expression.operatorToken.kind}`
+        );
+      }
+      if (isElementAccessExpression(expression.left)) {
+        let currentExpression: Node = [...[expression.left]][0];
+        const items: SkittlesExpression[] = [];
+        while (isElementAccessExpression(currentExpression)) {
+          items.unshift(
+            getSkittlesExpression((currentExpression as any).argumentExpression)
+          );
+          currentExpression = (currentExpression as any).expression;
+        }
+
+        if (isEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.MappingUpdate,
+            variable: getNodeName(currentExpression),
+            items,
+            value: getSkittlesExpression(expression.right),
+          };
+        }
+        if (isPlusEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.MappingUpdate,
+            variable: getNodeName(currentExpression),
+            items,
+            value: {
+              expressionType: SkittlesExpressionType.Binary,
+              operator: SkittlesOperator.Plus,
+              left: getSkittlesExpression(expression.left),
+              right: getSkittlesExpression(expression.right),
+            },
+          };
+        }
+        if (isMinusEquals(expression)) {
+          return {
+            statementType: SkittlesStatementType.MappingUpdate,
+            variable: getNodeName(currentExpression),
+            items,
+            value: {
+              expressionType: SkittlesExpressionType.Binary,
+              operator: SkittlesOperator.Minus,
+              left: getSkittlesExpression(expression.left),
+              right: getSkittlesExpression(expression.right),
+            },
+          };
+        }
+        throw new Error(
+          `Unknown element access expression: ${expression.operatorToken.kind}`
+        );
+      }
+      throw new Error(`Unknown binary expression type: ${expression.kind}`);
+    }
+    if (isCallExpression(expression)) {
+      return {
+        statementType: SkittlesStatementType.Call,
+        target: getNodeName(expression.expression),
+        parameters: expression.arguments.map(getSkittlesExpression),
+      };
+    }
+    throw new Error("Not implemented expression statement handling");
+  }
+  if (isReturnStatement(node)) {
+    const { expression } = node;
+    if (!expression) throw new Error("Return statement has no expression");
+    if (isBinaryExpression(expression)) {
+      return {
+        statementType: SkittlesStatementType.Return,
+        type: returnType,
+        value: getSkittlesExpression(expression),
+      };
+    }
+    if (isPropertyAccessExpression(expression)) {
+      return {
+        statementType: SkittlesStatementType.Return,
+        type: returnType,
+        value: getSkittlesExpression(expression),
+      };
+    }
+    if (isElementAccessExpression(expression)) {
+      return {
+        statementType: SkittlesStatementType.Return,
+        type: returnType,
+        value: getSkittlesExpression(expression),
+      };
+    }
+    if (isTrueKeyword(expression)) {
+      return {
+        statementType: SkittlesStatementType.Return,
+        type: returnType,
+        value: {
+          expressionType: SkittlesExpressionType.Value,
+          type: "bool",
+          value: "true",
+        },
+      };
+    }
+    if (isFalseKeyword(expression)) {
+      return {
+        statementType: SkittlesStatementType.Return,
+        type: returnType,
+        value: {
+          expressionType: SkittlesExpressionType.Value,
+          type: "bool",
+          value: "false",
+        },
+      };
+    }
+    throw new Error(`Unknown return expression type: ${expression.kind}`);
+  }
+  if (isIfStatement(node)) {
+    const { expression, thenStatement, elseStatement } = node;
+    if (!thenStatement) throw new Error("If statement has no then statement");
+    return {
+      statementType: SkittlesStatementType.If,
+      condition: getSkittlesExpression(expression),
+      then: getSkittlesStatements(thenStatement, "void"),
+      else: getSkittlesStatements(elseStatement, "void"),
+    };
+  }
+  if (isThrowStatement(node)) {
+    const { expression } = node;
+    if (isNewExpression(expression)) {
+      const args = expression.arguments;
+      if (!args) throw new Error("Throw statement has no arguments");
+      if (args.length === 0)
+        throw new Error("Throw statement has no arguments");
+      if (args.length > 1)
+        throw new Error("Throw statement has too many arguments");
+      return {
+        statementType: SkittlesStatementType.Throw,
+        error: getSkittlesExpression(args[0]),
+      };
+    }
+    throw new Error("Not implemented throw statement handling");
+  }
+  throw new Error(`Unknown statement type: ${node.kind}`);
+};
+
+export default getSkittlesStatements;
