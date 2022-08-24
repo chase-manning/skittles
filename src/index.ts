3f9e48a6c5f223563354cb2bdde0ef81b72bda41
chase-manning <chase@manning.dev>
2022-08-24 11:41:53 +0100

    :art: change to using yargs for cli

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-// import chalk from "chalk";
-// import clear from "clear";
 // import figlet from "figlet";
-import { program } from "commander";
+import yargs from "yargs";
 import getAbi from "./abi/get-abi";
 import getBytecode from "./bytecode/get-bytecode";
 import {
@@ -30,30 +28,16 @@ const skittlesCompile = async () => {
   await Promise.all(promises);
 };
 
-// clear();
-// console.log(
-//   chalk.red(figlet.textSync("skittles-cli", { horizontalLayout: "full" }))
-// );
+// Basic, bubbles, Coinstak, Colossal, Dot matrix, Roman
 
-program
-  .name("skittles")
-  .description("CLI for the Skittles compiler")
-  .version("0.1.0");
-
-program
-  .command("compile")
-  .description("Compile all TypeScript files")
-  .action(async () => {
+yargs
+  .usage("test usage")
+  .command("compile", "Compile all TypeScript files", async () => {
     await skittlesCompile();
-  });
-
-program
-  .command("clean")
-  .description("Clears the cache and deletes all builds")
-  .action(() => {
+  })
+  .command("clean", "Clears the cache and deletes all builds", () => {
     clearDirectory("./build");
-  });
-
-program.parse();
+  })
+  .parse();
 
 export { address, self, block, chain, msg, tx, getSkittlesFactory };
