611536dc34fe7ac99e4ad580c65affa4fc7b3f78
chase-manning <chase@manning.dev>
2022-08-24 10:19:49 +0100

    :bug: skittles factory not working

--- src/testing/get-skittles-factory.ts
+++ src/testing/get-skittles-factory.ts
@@ -1,17 +1,19 @@
+import { ContractFactory } from "ethers";
 import fs from "fs";
 import { Abi } from "../types/abi-types";
-import { ContractFactory } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 const getFileString = (fileName: string) => {
   return fs.readFileSync(fileName, { encoding: "utf8" });
 };
 
 const getSkittlesFactory = async (
+  signer: SignerWithAddress,
   contract: string
 ): Promise<ContractFactory> => {
   const abi = JSON.parse(getFileString(`./build/abi/${contract}.abi`)) as Abi;
-  const bytecode = getFileString(`./build/bytecode/${contract}.bin`);
-  return new ContractFactory(abi, bytecode);
+  const bytecode = getFileString(`./build/bytecode/${contract}.bytecode`);
+  return new ContractFactory(abi, bytecode, signer);
 };
 
 export default getSkittlesFactory;
