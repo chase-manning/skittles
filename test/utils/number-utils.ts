4fe90957371c6fe560f78a65c3ae306793d54dfb
chase-manning <chase@manning.dev>
2022-08-25 12:54:04 +0100

    :sparkles: add testing number utils

---
+++ test/utils/number-utils.ts
@@ -0,0 +1,8 @@
+import { BigNumber } from "ethers";
+
+export const scale = (
+  value: number | string,
+  decimals: number = 18
+): BigNumber => {
+  return BigNumber.from(value).mul(BigNumber.from(10).pow(decimals));
+};
