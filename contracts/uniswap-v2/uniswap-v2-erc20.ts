5c260a508eeda6ad6485066a50222527b2632edd
chase-manning <chase@manning.dev>
2022-08-25 10:43:28 +0100

    :sparkles: add balanceOf and allowance to uniswap erc20

--- contracts/uniswap-v2/uniswap-v2-erc20.ts
+++ contracts/uniswap-v2/uniswap-v2-erc20.ts
@@ -1,6 +1,10 @@
+import { address } from "../../src/types/core-types";
+
 export class UniswapV2ERC20 {
   readonly name: string = "Uniswap V2";
   readonly symbol: string = "UNI-V2";
   readonly decimals: number = 18;
   totalSupply: number;
+  balanceOf: Record<address, number>;
+  allownace: Record<address, Record<address, number>>;
 }
