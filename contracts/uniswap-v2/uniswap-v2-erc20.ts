9c5ca9a93a5c01000ccd8e237d0447d1c92a265a
chase-manning <chase@manning.dev>
2022-08-25 11:25:25 +0100

    :sparkles: complete uniswap v2 erc20 contract

--- contracts/uniswap-v2/uniswap-v2-erc20.ts
+++ contracts/uniswap-v2/uniswap-v2-erc20.ts
@@ -1,4 +1,4 @@
-import { address } from "../../src/types/core-types";
+import { address, msg } from "../../src/types/core-types";
 
 export class UniswapV2ERC20 {
   readonly name: string = "Uniswap V2";
@@ -7,4 +7,42 @@ export class UniswapV2ERC20 {
   totalSupply: number;
   balanceOf: Record<address, number>;
   allownace: Record<address, Record<address, number>>;
+
+  private _mint(to: address, amount: number): void {
+    this.balanceOf[to] += amount;
+    this.totalSupply += amount;
+  }
+
+  private _burn(from: address, amount: number): void {
+    this.balanceOf[from] -= amount;
+    this.totalSupply -= amount;
+  }
+
+  private _approve(from: address, to: address, amount: number): void {
+    this.allownace[from][to] = amount;
+  }
+
+  private _transfer(from: address, to: address, amount: number): void {
+    this.balanceOf[to] += amount;
+    this.balanceOf[from] -= amount;
+  }
+
+  approve(spender: address, amount: number): boolean {
+    this._approve(msg.sender, spender, amount);
+    return true;
+  }
+
+  transfer(to: address, amount: number): boolean {
+    this._transfer(msg.sender, to, amount);
+    return true;
+  }
+
+  transferFrom(from: address, to: address, amount: number): boolean {
+    if (this.allownace[from][msg.sender] < amount) {
+      throw new Error("amount exceeds allowance");
+    }
+    this._transfer(from, to, amount);
+    this.allownace[from][msg.sender] -= amount;
+    return true;
+  }
 }
