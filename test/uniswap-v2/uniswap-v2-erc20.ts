6fad674f77466a3beadd4745aca10cdfd866e7f5
chase-manning <chase@manning.dev>
2022-08-29 08:30:54 -0400

    ✅ add full test suite for uniswap v2 erc20

--- test/uniswap-v2/uniswap-v2-erc20.ts
+++ test/uniswap-v2/uniswap-v2-erc20.ts
@@ -4,19 +4,19 @@ import { Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import getSkittlesFactory from "../../src/testing/get-skittles-factory";
 import { solidity } from "ethereum-waffle";
-import { scale } from "../utils/number-utils";
+import { MAX_UINT256, scale } from "../utils/number-utils";
 
 chai.use(solidity);
 
 const TOTAL_SUPPLY = scale(10000);
-// const TEST_AMOUNT = expandTo18Decimals(10);
+const TEST_AMOUNT = scale(10);
 
-describe("ERC20", () => {
+describe("Uniswap V2 ERC20", () => {
   let token: Contract;
   let wallet: SignerWithAddress;
   let other: SignerWithAddress;
 
-  before(async () => {
+  beforeEach(async () => {
     let signers = await ethers.getSigners();
     wallet = signers[0];
     other = signers[1];
@@ -31,125 +31,76 @@ describe("ERC20", () => {
     expect(name).to.eq("Uniswap V2");
     expect(await token.symbol()).to.eq("UNI-V2");
     expect(await token.decimals()).to.eq(18);
+    expect(await token.balanceOf(other.address)).to.eq(0);
     expect(await token.balanceOf(wallet.address)).to.eq(TOTAL_SUPPLY);
-    //     expect(await token.DOMAIN_SEPARATOR()).to.eq(
-    //       keccak256(
-    //         defaultAbiCoder.encode(
-    //           ["bytes32", "bytes32", "bytes32", "uint256", "address"],
-    //           [
-    //             keccak256(
-    //               toUtf8Bytes(
-    //                 "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
-    //               )
-    //             ),
-    //             keccak256(toUtf8Bytes(name)),
-    //             keccak256(toUtf8Bytes("1")),
-    //             1,
-    //             token.address,
-    //           ]
-    //         )
-    //       )
-    //     );
-    //     expect(await token.PERMIT_TYPEHASH()).to.eq(
-    //       keccak256(
-    //         toUtf8Bytes(
-    //           "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
-    //         )
-    //       )
-    //     );
+    expect(await token.balanceOf(other.address)).to.eq(0);
   });
 
-  //   it("approve", async () => {
-  //     await expect(token.approve(other.address, TEST_AMOUNT))
-  //       .to.emit(token, "Approval")
-  //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
-  //     expect(await token.allowance(wallet.address, other.address)).to.eq(
-  //       TEST_AMOUNT
-  //     );
-  //   });
+  it("approve", async () => {
+    //     await expect(token.approve(other.address, TEST_AMOUNT))
+    //       .to.emit(token, "Approval")
+    //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
+    await token.approve(other.address, TEST_AMOUNT);
+    expect(await token.allowance(wallet.address, other.address)).to.eq(
+      TEST_AMOUNT
+    );
+  });
 
-  //   it("transfer", async () => {
-  //     await expect(token.transfer(other.address, TEST_AMOUNT))
-  //       .to.emit(token, "Transfer")
-  //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
-  //     expect(await token.balanceOf(wallet.address)).to.eq(
-  //       TOTAL_SUPPLY.sub(TEST_AMOUNT)
-  //     );
-  //     expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
-  //   });
+  it("transfer", async () => {
+    //     await expect(token.transfer(other.address, TEST_AMOUNT))
+    //       .to.emit(token, "Transfer")
+    //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
+    await token.transfer(other.address, TEST_AMOUNT);
+    expect(await token.balanceOf(wallet.address)).to.eq(
+      TOTAL_SUPPLY.sub(TEST_AMOUNT)
+    );
+    expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
+  });
 
-  //   it("transfer:fail", async () => {
-  //     await expect(token.transfer(other.address, TOTAL_SUPPLY.add(1))).to.be
-  //       .reverted; // ds-math-sub-underflow
-  //     await expect(token.connect(other).transfer(wallet.address, 1)).to.be
-  //       .reverted; // ds-math-sub-underflow
-  //   });
+  it("transfer:fail", async () => {
+    await expect(token.transfer(other.address, TOTAL_SUPPLY.add(1))).to.be
+      .reverted; // ds-math-sub-underflow
+    await expect(token.connect(other).transfer(wallet.address, 1)).to.be
+      .reverted; // ds-math-sub-underflow
+  });
 
-  //   it("transferFrom", async () => {
-  //     await token.approve(other.address, TEST_AMOUNT);
-  //     await expect(
-  //       token
-  //         .connect(other)
-  //         .transferFrom(wallet.address, other.address, TEST_AMOUNT)
-  //     )
-  //       .to.emit(token, "Transfer")
-  //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
-  //     expect(await token.allowance(wallet.address, other.address)).to.eq(0);
-  //     expect(await token.balanceOf(wallet.address)).to.eq(
-  //       TOTAL_SUPPLY.sub(TEST_AMOUNT)
-  //     );
-  //     expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
-  //   });
+  it("transferFrom", async () => {
+    await token.approve(other.address, TEST_AMOUNT);
+    //     await expect(
+    //       token
+    //         .connect(other)
+    //         .transferFrom(wallet.address, other.address, TEST_AMOUNT)
+    //     )
+    //       .to.emit(token, "Transfer")
+    //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
+    await token
+      .connect(other)
+      .transferFrom(wallet.address, other.address, TEST_AMOUNT);
+    expect(await token.allowance(wallet.address, other.address)).to.eq(0);
+    expect(await token.balanceOf(wallet.address)).to.eq(
+      TOTAL_SUPPLY.sub(TEST_AMOUNT)
+    );
+    expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
+  });
 
-  //   it("transferFrom:max", async () => {
-  //     await token.approve(other.address, MaxUint256);
-  //     await expect(
-  //       token
-  //         .connect(other)
-  //         .transferFrom(wallet.address, other.address, TEST_AMOUNT)
-  //     )
-  //       .to.emit(token, "Transfer")
-  //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
-  //     expect(await token.allowance(wallet.address, other.address)).to.eq(
-  //       MaxUint256
-  //     );
-  //     expect(await token.balanceOf(wallet.address)).to.eq(
-  //       TOTAL_SUPPLY.sub(TEST_AMOUNT)
-  //     );
-  //     expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT);
-  //   });
-
-  //   it("permit", async () => {
-  //     const nonce = await token.nonces(wallet.address);
-  //     const deadline = MaxUint256;
-  //     const digest = await getApprovalDigest(
-  //       token,
-  //       { owner: wallet.address, spender: other.address, value: TEST_AMOUNT },
-  //       nonce,
-  //       deadline
-  //     );
-
-  //     const { v, r, s } = ecsign(
-  //       Buffer.from(digest.slice(2), "hex"),
-  //       Buffer.from(wallet.privateKey.slice(2), "hex")
-  //     );
-
-  //     await expect(
-  //       token.permit(
-  //         wallet.address,
-  //         other.address,
-  //         TEST_AMOUNT,
-  //         deadline,
-  //         v,
-  //         hexlify(r),
-  //         hexlify(s)
-  //       )
-  //     )
-  //       .to.emit(token, "Approval")
-  //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
-  //     expect(await token.allowance(wallet.address, other.address)).to.eq(
-  //       TEST_AMOUNT
-  //     );
-  //     expect(await token.nonces(wallet.address)).to.eq(bigNumberify(1));
-  //   });
+  it("transferFrom:max", async () => {
+    await token.approve(other.address, MAX_UINT256);
+    //     await expect(
+    //       token
+    //         .connect(other)
+    //         .transferFrom(wallet.address, other.address, TEST_AMOUNT)
+    //     )
+    //       .to.emit(token, "Transfer")
+    //       .withArgs(wallet.address, other.address, TEST_AMOUNT);
+    await token
+      .connect(other)
+      .transferFrom(wallet.address, other.address, TEST_AMOUNT);
+    expect(await token.allowance(wallet.address, other.address)).to.eq(
+      MAX_UINT256
+    );
+    expect(await token.balanceOf(wallet.address)).to.eq(
+      TOTAL_SUPPLY.sub(TEST_AMOUNT)
+    );
+    expect(await token.balanceOf(other.address)).to.eq(TEST_AMOUNT); //
+  });
 });
