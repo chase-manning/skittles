1038c0d38442fab7c239ee145a6f89d991df6fd5
chase-manning <chase@manning.dev>
2022-09-02 12:55:20 -0400

    :sparkles: add create pair function to uniswap v2 factory

--- contracts/uniswap-v2/uniswap-v2-factory.ts
+++ contracts/uniswap-v2/uniswap-v2-factory.ts
@@ -1,6 +1,8 @@
 import { address } from "../../src/types/core-types";
 
 // TODO Add events
+// TODO Refactor token assignments to use conditional expressions a ? b : c
+// TODO Increase line length and refactor to assing token0 and token1 at the same time
 
 export class UniswapV2Factory {
   feeTo: address;
@@ -15,5 +17,17 @@ export class UniswapV2Factory {
 
   allPairsLength(): number {
     return this.allPairs.length;
+  }
+
+  createPair(tokenA: address, tokenB: address): void {
+    if (tokenA === tokenB) {
+      throw new Error("UniswapV2: IDENTICAL_ADDRESSES");
+    }
+    let token0 = tokenA;
+    let token1 = tokenB;
+    if (tokenA > tokenB) {
+      token0 = tokenB;
+      token1 = tokenA;
+    }
   }
 }
