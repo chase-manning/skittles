9dc123973ee4b53fa2deedeb89bfa9ce051d79ab
chase-manning <chase@manning.dev>
2022-09-11 20:58:39 +0100

    :art: use ZERO_ADDRESS import for uniswap v2 factory

--- contracts/uniswap-v2/uniswap-v2-factory.ts
+++ contracts/uniswap-v2/uniswap-v2-factory.ts
@@ -1,8 +1,8 @@
+import { ZERO_ADDRESS } from "../../src/data/constants";
 import { address } from "../../src/types/core-types";
 
 // TODO Add events
 // TODO Increase line length and refactor to assing token0 and token1 at the same time
-// TODO Change to using ZERO_ADDRESS import
 
 export class UniswapV2Factory {
   feeTo: address;
@@ -25,13 +25,10 @@ export class UniswapV2Factory {
     }
     let token0 = tokenA > tokenB ? tokenB : tokenA;
     let token1 = tokenA > tokenB ? tokenA : tokenB;
-    if (token0 === "0x0000000000000000000000000000000000000000") {
+    if (token0 === ZERO_ADDRESS) {
       throw new Error("UniswapV2: ZERO_ADDRESS");
     }
-    if (
-      this.getPair[token0][token1] !==
-      "0x0000000000000000000000000000000000000000"
-    ) {
+    if (this.getPair[token0][token1] !== ZERO_ADDRESS) {
       throw new Error("UniswapV2: PAIR_EXISTS");
     }
     // const pair = new UniswapV2Pair();
