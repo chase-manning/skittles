11b2c0a0b640a634c89a550a1865eeb4234de904
chase-manning <chase@manning.dev>
2022-09-12 15:49:29 +0100

    :art: move erc20 contract into token directory

--- contracts/token/erc20.ts
+++ contracts/token/erc20.ts
@@ -1,5 +1,21 @@
-import { address, msg } from "../src/types/core-types";
+import { address, msg } from "../../src/types/core-types";
+// import IERC20 from "./ierc20";
 
+// TODO Move events to interrace
+
+// interface TransferEvent {
+//   from: address;
+//   to: address;
+//   amount: number;
+// }
+
+// interface ApprovalEvent {
+//   owner: address;
+//   spender: address;
+//   amount: number;
+// }
+
+// export class ERC20 implements IERC20 {
 export class ERC20 {
   readonly decimals: number = 18;
   readonly symbol: string = "TEST";
@@ -9,6 +25,9 @@ export class ERC20 {
   balanceOf: Record<address, number>;
   allowance: Record<address, Record<address, number>>;
 
+  // transferEvent: SkittlesEvent<TransferEvent>;
+  // approvalEvent: SkittlesEvent<ApprovalEvent>;
+
   constructor(mintAmount_: number) {
     this.balanceOf[msg.sender] = mintAmount_;
     this.totalSupply = mintAmount_;
@@ -16,6 +35,7 @@ export class ERC20 {
 
   approve(spender: address, amount: number): boolean {
     this.allowance[msg.sender][spender] = amount;
+    // this.approvalEvent.emit({ owner: msg.sender, spender, amount });
     return true;
   }
 
@@ -35,5 +55,6 @@ export class ERC20 {
   private _transfer(from: address, to: address, amount: number): void {
     this.balanceOf[to] += amount;
     this.balanceOf[from] -= amount;
+    // this.transferEvent.emit({ from, to, amount });
   }
 }
