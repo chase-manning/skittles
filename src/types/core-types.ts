554b8a0e4738a3907f96d6e5a02c3f981b137ed2
chase-manning <chase@manning.dev>
2022-09-24 14:33:40 +0100

    :sparkles: add skittles config type

--- src/types/core-types.ts
+++ src/types/core-types.ts
@@ -68,3 +68,56 @@ export class tx {
 export interface SkittlesEvent<Type> {
   emit(data: Type): void;
 }
+
+export interface SkittlesConfig {
+  // Optional: Optimizer settings
+  optimizer?: {
+    // Disabled by default.
+    // NOTE: enabled=false still leaves some optimizations on. See comments below.
+    // WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+    // it to false and would actually disable all the optimizations.
+    enabled?: boolean;
+    // Optimize for how many times you intend to run the code.
+    // Lower values will optimize more for initial deployment cost, higher
+    // values will optimize more for high-frequency usage.
+    runs?: number;
+    // Switch optimizer components on or off in detail.
+    // The "enabled" switch above provides two defaults which can be
+    // tweaked here. If "details" is given, "enabled" can be omitted.
+    details?: {
+      // The peephole optimizer is always on if no details are given,
+      // use details to switch it off.
+      peephole?: boolean;
+      // The inliner is always on if no details are given,
+      // use details to switch it off.
+      inliner?: boolean;
+      // The unused jumpdest remover is always on if no details are given,
+      // use details to switch it off.
+      jumpdestRemover?: boolean;
+      // Sometimes re-orders literals in commutative operations.
+      orderLiterals?: boolean;
+      // Removes duplicate code blocks
+      deduplicate?: boolean;
+      // Common subexpression elimination, this is the most complicated step but
+      // can also provide the largest gain.
+      cse?: boolean;
+      // Optimize representation of literal numbers and strings in code.
+      constantOptimizer?: boolean;
+      // The new Yul optimizer. Mostly operates on the code of ABI coder v2
+      // and inline assembly.
+      // It is activated together with the global optimizer setting
+      // and can be deactivated here.
+      // Before Solidity 0.6.0 it had to be activated through this switch.
+      yul?: boolean;
+      // Tuning options for the Yul optimizer.
+      yulDetails?: {
+        // Improve allocation of stack slots for variables, can free up stack slots early.
+        // Activated by default if the Yul optimizer is activated.
+        stackAllocation?: boolean;
+        // Select optimization steps to be applied.
+        // Optional, the optimizer will use the default sequence if omitted.
+        optimizerSteps?: string;
+      };
+    };
+  };
+}
