70ebba59ec551679b72a95fc536671d08eb4b171
Chase Manning <chase@manning.dev>
2026-03-13 17:13:54 -0300

    fix: address review feedback

--- constants/debounce.d.ts
+++
@@ -1,3 +0,0 @@
-/** Debounce delay (ms) for recompilation triggers (file-watch & playground). */
-export declare const DEBOUNCE_MS = 300;
-//# sourceMappingURL=debounce.d.ts.map
--- constants/debounce.js
+++
@@ -1,3 +0,0 @@
-/** Debounce delay (ms) for recompilation triggers (file-watch & playground). */
-export const DEBOUNCE_MS = 300;
-//# sourceMappingURL=debounce.js.map
